366395880ad302212ea9ac24835c11bdec8d92f6
X-Zero-L <zeroeeau@gmail.com>
2024-10-15 16:29:41 +0800

    Refactor next.config.mjs to add support for hypermax.app API URLs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
             port: '',
             pathname: '/**',
           },
+          {
+            protocol: 'https',
+            hostname: '*hypermax.app',
+            port: '',
+            pathname: '/**',
+          }
         ],
       },
 };
